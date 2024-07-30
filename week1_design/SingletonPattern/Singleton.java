d5e57234ab0bdf6cfdce02259a240d7d644a2c81
Neelgats <141011422+Neelgats@users.noreply.github.com>
2024-07-30 23:26:08 +0530

    Delete week1_design/SingletonPattern directory

--- week1_design/SingletonPattern/Singleton.java
+++
@@ -1,9 +0,0 @@
-public class Singleton {
-    public static void main(String[] args) {
-        Logger log1=Logger.getInstance();
-        Logger log2=Logger.getInstance();
-
-        log1.log("The first log.");
-        log2.log("The second log.");
-    }
-}
